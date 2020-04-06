89a5a0debdcfe0f6ebabab3a1d8e1eb7ab0ebe06
Ivan Preziosi <ivan.preziosi@gmail.com>
2020-04-06 15:53:19 +0200

    login

--- routes/user.js
+++ routes/user.js
@@ -29,7 +29,7 @@ router.post('/save',
       .withMessage('must be at least 6 chars long and not longer than 50 chars'),
 
     //password_repeat
-    body('password_repeat')      
+    body('password_repeat')
       .exists().withMessage('is required')
       .bail()
       .notEmpty().withMessage('is required')
@@ -84,7 +84,7 @@ router.post('/save',
 
     } else {
       // SAVING NEW USER
-      console.log("saving NEW USER: "+req.body.username);
+      console.log("saving NEW USER: " + req.body.username);
 
       var username = req.body.username;
       var password = req.body.password;
@@ -101,7 +101,7 @@ router.post('/save',
         } else {
           console.log("new user saved");
 
-          var leaderboardResponse = {
+          var userResponse = {
             status: "OK",
             message: "User registered successfully",
             code: "LEADERBOARD-NEW-USER-SAVED-SUCCESS"
@@ -109,10 +109,114 @@ router.post('/save',
 
           //write to result object
           res.writeHead(200, { 'Content-Type': 'text/json' });
-          res.write(JSON.stringify(leaderboardResponse));
+          res.write(JSON.stringify(userResponse));
           res.end();
         }
 
+      });
+    }
+  });
+
+
+
+
+/* POST user login */
+router.post('/login',
+  [ //express-validator rules stack
+
+    //username
+    body('username')
+      .exists().withMessage('is required')
+      .bail()
+      .trim()
+      .notEmpty().withMessage('is required')
+      .bail()
+      .isLength({ min: 3, max: 50 })
+      .withMessage('must be at least 3 chars long and not longer than 50 chars'),
+
+    //password
+    body('password')
+      .exists().withMessage('is required')
+      .bail()
+      .notEmpty().withMessage('is required')
+      .bail()
+      .isLength({ min: 6, max: 50 })
+      .withMessage('must be at least 6 chars long and not longer than 50 chars'),
+
+  ],
+  function (req, res, next) {
+
+    //VALIDATION CHECK
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+
+      console.log(JSON.stringify(errors));
+
+      var errorString = "Errors: ";
+      var isFirst = true;
+      errors.array().forEach(element => {
+        if (!isFirst) {
+          errorString += " - ";
+        }
+        errorString += element.param + ": " + element.msg;
+        isFirst = false;
+      });
+
+      //create 400 http error code: Bad Request
+      next(createError(400, errorString));
+
+    } else {
+      // logging in user
+      console.log("logging in USER: " + req.body.username);
+
+      var username = req.body.username;
+      var password = req.body.password;
+
+      var con = require('../app_modules/DBConnection');
+      var sql = "SELECT * FROM `user` WHERE username = ? AND password = ? LIMIT 1";
+
+      con.query(sql, [username, md5(password)], function (query_err, result, fields) {
+
+        if (query_err) {
+          console.log("ERR")
+          next(query_err);
+        } else {
+
+          if (result.length != 1) {
+            next(new Error('User not found.'))
+          } else {
+
+            var salt = "asdasdjhaslkdsàsdflsfj9sdfhjksd/(&%/(&_874hcdcchui32bj3sc98csd_-sdhfj!*£%&";
+            var token = md5(result[0].username + salt + result[0].password + salt + result[0].date_subscribed + Date());
+
+            var sqlUpdate = "UPDATE `node_leaderboard_lite`.`user` SET `auth_token`=? WHERE  `id`=" + result[0].id;
+
+            con.query(sqlUpdate, [token], function (query_err, result, fields) {
+
+              if (query_err) {
+                console.log("ERR")
+                next(query_err);
+              } else {
+
+                var userResponse = {
+                  status: "OK",
+                  message: "User logged in successfully",
+                  code: "LEADERBOARD-USER-LOGIN-SUCCESS",
+                  payload: {
+                    "username": username,
+                    "token": token
+                  }
+                }
+
+                //write to result object
+                res.writeHead(200, { 'Content-Type': 'text/json' });
+                res.write(JSON.stringify(userResponse));
+                res.end();
+              }
+            });
+          }
+        }
       });
     }
   });
