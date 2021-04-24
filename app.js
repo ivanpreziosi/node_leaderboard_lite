6110d33f4fae1205add0492b3a7891dbec8877da
Ivan Preziosi <ivan.preziosi@gmail.com>
2021-04-24 09:16:20 +0200

    docs

--- app.js
+++ app.js
@@ -14,19 +14,19 @@ var app = express();
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
-app.use(function(req,res,next){
-  if (envConfig.error) {
-    next(createError(500,envConfig.error)); 
-  }
-  next();
+app.use(function(req, res, next) {
+    if (envConfig.error) {
+        next(createError(500, envConfig.error));
+    }
+    next();
 });
 
-app.use(['/ldb'],function (req, res, next) {
-  Auth.checkAuth(req, res, next);
+app.use(['/ldb'], function(req, res, next) {
+    Auth.checkAuth(req, res, next);
 });
 
-app.use('/usr/logout',function (req, res, next) {
-  Auth.checkAuth(req, res, next);
+app.use('/usr/logout', function(req, res, next) {
+    Auth.checkAuth(req, res, next);
 });
 
 //catch all calls to ldb and send them to leaderboard router!!
@@ -35,27 +35,27 @@ app.use('/ldb', leaderboardRouter);
 //routing delle funzioni per gli endpoint di usr
 app.use('/usr', userRouter);
 
-// catch everything not catched before as a 404 and forward to error handler
 app.use(function(req, res, next) {
-  console.log('404');
-  next(createError(404,"The requested resource is non existent!!"));
+    //__dirname is an environment variable that tells you the absolute path of the directory containing the currently executing file!
+    var dirPath = __dirname;
+    res.sendFile(dirPath + '/views/doc.html');
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error message (500 defaults to internal error)
-  res.status(err.status || 500);
-  res.json({
-    "status": "KO",
-    "message": err.message,
-    "code": "ERROR-HANDLE-"+(err.status || 500)
-  });
-  res.end();
-  
+    // render the error message (500 defaults to internal error)
+    res.status(err.status || 500);
+    res.json({
+        "status": "KO",
+        "message": err.message,
+        "code": "ERROR-HANDLE-" + (err.status || 500)
+    });
+    res.end();
+
 });
 
 module.exports = app;
